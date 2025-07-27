3a6e3559b3861edfbbe6e86a516dd34fc2141dae
flarycen <123115894+flerisen@users.noreply.github.com>
2025-07-27 14:28:30 +0300

    Randomized computer choice and prompting for Human input.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,29 @@
 console.log("hello external js");
+
+// Create a function for Computer player by leveraging the Math.random function
+
+function getComputerChoice() {
+    if (Math.random() <= 0.33) {
+        let choiceComputer = "Rock";
+        console.log("Rock");
+    } else if ( 0.33 < Math.random() < 0.66) {
+        let choiceComputer = "Paper";
+        console.log("Paper");
+    } else {
+        let choiceComputer = "Scissors";
+        console.log("Scissors");
+        
+    }
+}
+
+getComputerChoice()
+
+// Create a function for Human player by leveraging the prompt ability
+
+function getHumanChoice() {
+    let choiceHuman = prompt("Please choose Rock, Paper or Scissors");
+    console.log(choiceHuman);
+    return choiceHuman;
+}
+
+getHumanChoice()
