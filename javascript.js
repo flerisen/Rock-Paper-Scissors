5466cc78e37642a3327a606d4ab48905c92f5263
flarycen <123115894+flerisen@users.noreply.github.com>
2025-07-27 14:34:24 +0300

    Created variables for score, differentiated human and computer output and made human output to be shown before computer output to block cheating.

--- javascript.js
+++ javascript.js
@@ -1,29 +1,28 @@
-console.log("hello external js");
-
 // Create a function for Computer player by leveraging the Math.random function
 
 function getComputerChoice() {
     if (Math.random() <= 0.33) {
         let choiceComputer = "Rock";
-        console.log("Rock");
+        console.log("Computer chose: Rock");
     } else if ( 0.33 < Math.random() < 0.66) {
         let choiceComputer = "Paper";
-        console.log("Paper");
+        console.log("Computer chose: Paper");
     } else {
         let choiceComputer = "Scissors";
-        console.log("Scissors");
-        
+        console.log("Computer chose: Scissors");
     }
 }
-
-getComputerChoice()
 
 // Create a function for Human player by leveraging the prompt ability
 
 function getHumanChoice() {
     let choiceHuman = prompt("Please choose Rock, Paper or Scissors");
-    console.log(choiceHuman);
+    console.log("Human chose: " + choiceHuman);
+    getComputerChoice();
     return choiceHuman;
 }
 
 getHumanChoice()
+
+let humanScore = 0;
+let computerScore = 0;
