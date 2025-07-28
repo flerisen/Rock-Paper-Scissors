a6b1f946f48b20a2d3de831636b8e3f14da74c00
flarycen <123115894+flerisen@users.noreply.github.com>
2025-07-28 12:56:49 +0300

    implemented a new function for the entire round and changed user input to case-insensitive

--- javascript.js
+++ javascript.js
@@ -1,28 +1,41 @@
 // Create a function for Computer player by leveraging the Math.random function
 
 function getComputerChoice() {
+    let choiceComputer;
+
     if (Math.random() <= 0.33) {
-        let choiceComputer = "Rock";
+        choiceComputer = "Rock";
         console.log("Computer chose: Rock");
-    } else if ( 0.33 < Math.random() < 0.66) {
-        let choiceComputer = "Paper";
+    } else if (Math.random() <= 0.66) {
+        choiceComputer = "Paper";
         console.log("Computer chose: Paper");
     } else {
-        let choiceComputer = "Scissors";
+        choiceComputer = "Scissors";
         console.log("Computer chose: Scissors");
     }
+    return choiceComputer;
 }
 
 // Create a function for Human player by leveraging the prompt ability
 
 function getHumanChoice() {
     let choiceHuman = prompt("Please choose Rock, Paper or Scissors");
+    choiceHuman = choiceHuman.toUpperCase().charAt(0) + choiceHuman.slice(1).toLowerCase();
     console.log("Human chose: " + choiceHuman);
-    getComputerChoice();
     return choiceHuman;
 }
 
-getHumanChoice()
-
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(choiceComputer, choiceHuman) {
+
+    if (choiceHuman === choiceComputer) {
+        console.log("Tie. Play again!");
+    }   
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
